defa87b182b7cbd884f8b6ea089d3b0fbfdb946e
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-03-21 14:27:25 -0400

    functions

---
+++ ToDoListVisionApp/ToDoListManager.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListManager.swift
+//  ToDoListVisionApp
+//
+//  Created by Davaughn Williams on 3/21/25.
+//
+
+import SwiftUI
+
+struct ToDoListManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListManager()
+}
