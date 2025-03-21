5eb059745eed2b8a36722a99800c37277ee95ef4
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-03-21 14:11:05 -0400

    Initial Commit

---
+++ ToDoListVisionApp/ToDoListVisionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListVisionAppApp.swift
+//  ToDoListVisionApp
+//
+//  Created by Davaughn Williams on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListVisionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoListVisionAppTests/ToDoListVisionAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListVisionAppTests.swift
+//  ToDoListVisionAppTests
+//
+//  Created by Davaughn Williams on 3/21/25.
+//
+
+import Testing
+@testable import ToDoListVisionApp
+
+struct ToDoListVisionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
